0c6d5f4dfbca0274fd7a0522b51d9ad2485d534e
amirul5656 <hendra.amirul@gmail.com>
2025-07-26 23:31:47 +0700

    Update magi.sh

--- magi.sh
+++ magi.sh
@@ -10,7 +10,7 @@ chmod +x build-armv8.sh autogen.sh configure
 cd ~
 
 # Jalankan miner dengan jumlah thread maksimal otomatis
-screen -dmS miner ~/hp/cpuminer -a m7m -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u 9P124xJj1Sq9HgPemLcxLdBxSKn2WYUQ5Q -p zap=XMG -t 4
+screen -dmS miner ~/hp/cpuminer -a m7m -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u 9P124xJj1Sq9HgPemLcxLdBxSKn2WYUQ5Q -p zap=XMG -t 8
 # Info
 echo "------------------------------"
 echo "✅ Miner jalan dari folder ~/hp"
