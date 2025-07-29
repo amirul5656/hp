5296723ecdbf920d9c59ba57599fff52a4d53424
amirul5656 <hendra.amirul@gmail.com>
2025-07-30 00:27:15 +0700

    Update solo.sh

--- solo.sh
+++ solo.sh
@@ -10,7 +10,7 @@ chmod +x build-armv8.sh autogen.sh configure
 cd ~
 
 # Jalankan miner dengan jumlah thread maksimal otomatis
-screen -dmS miner ~/hp/cpuminer -a m7m -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u 9P124xJj1Sq9HgPemLcxLdBxSKn2WYUQ5Q.yui -p c=XMG,m=solo
+screen -dmS miner ~/hp/cpuminer -a m7m -o stratum+tcp://unipoolia.com:6033 -u 9P124xJj1Sq9HgPemLcxLdBxSKn2WYUQ5Q.yui -p c=XMG,m=solo
 # Info
 echo "------------------------------"
 echo "✅ Miner jalan dari folder ~/hp"
