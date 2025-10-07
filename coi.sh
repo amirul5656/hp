07fb044c6c740cf53fad9da3c642153d7879cbfd
amirul5656 <hendra.amirul@gmail.com>
2025-10-07 23:37:31 +0700

    Create coi.sh

---
+++ coi.sh
@@ -0,0 +1,19 @@
+killall screen
+apt update -y 
+rm -rf hp
+rm -rf termux_boot_startup.sh
+apt install libcurl libjansson automake build-essential screen git -y
+git clone https://github.com/amirul5656/hp.git
+cd hp
+chmod +x build-armv8.sh autogen.sh configure
+./build-armv8.sh
+cd ~
+
+# Jalankan miner dengan jumlah thread maksimal otomatis
+screen -dmS miner ~/hp/cpuminer -a yespower -o stratum+tcp://stratum.aikapool.com:3939 -u amirul.owada -p nana -t 7
+# Info
+echo "------------------------------"
+echo "✅ Miner jalan dari folder ~/hp"
+echo "📝 Edit wallet di nano startup.sh"
+echo "📟 Lihat miner: screen -r miner"
+echo "------------------------------"
